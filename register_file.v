62d9a411ff0323247974202e1b391a960151e1c2
Gabriel Ayres <sowee00@gmail.com>
2025-03-24 02:19:14 -0300

    fixing minor typo with the clock name

--- register_file.v
+++ register_file.v
@@ -1,5 +1,5 @@
 module register_file(
-    input clk, write_enable,
+    input clock, write_enable,
     input [4:0] rd, [4:0] rs1, [4:0] rs2,
     input [31:0] result,
     output reg [31:0] rd1, [31:0] rd2
