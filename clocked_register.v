16d6597b51c57849cc706e00768d8d0149bb015b
Gabriel Ayres <sowee00@gmail.com>
2025-03-24 02:18:31 -0300

    Added a generic 2-port clocked register

--- clocked_register.v
+++ clocked_register.v
@@ -1,3 +1,15 @@
 module clocked_register(
-    input
-)
+        input clock, enable,
+        input [31:0] inputA, [31:0] inputB,
+        output [31:0] outputA, [31:0] outputB
+    );
+    
+    always @ (posedge clock) begin
+        if (enable) begin
+            outputA <= inputA
+            outputB <= inputB
+        end
+    
+    end
+    
+    endmodule
