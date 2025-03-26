e4f4ecfbe08be7940451e99caa0823c67b5b9d30
Gabriel Ayres <sowee00@gmail.com>
2025-03-26 02:30:07 -0300

    Full control unit implementation

--- control_unit.v
+++ control_unit.v
@@ -12,9 +12,36 @@ module control_unit(
     output immediate_source[1:0]
 );
 
-reg state 4'b0000;
+reg state = 4'b0000;
+reg pc_update = 1'b0;
+reg branch = 1'b0;
+reg ALU_operation = 2'b00;
+
 
 always @ (posedge clock) begin
+
+    //ALU Decoder
+    case(ALU_operation)
+        2'b00: ALU_control = 2'b000;
+        2'b01: ALU_control = 2'b001;
+        2'b10: begin
+            case(funct3)
+                3'b010: ALU_control = 3'b101;
+                3'b110: ALU_control = 3'b110;
+                3'b111: ALU_control = 3'b011;
+                3'b000: begin
+                    case({opcode[5],funct7[5]})
+                        2'b00, 2'b01, 2'b10: ALU_control = 3'b000;
+                        2'b11: ALU_control = 3'b001;
+                    endcase
+                end
+            endcase
+        end
+    endcase
+
+    //pc write control signal
+    pc_write = (zero && branch) || pc_update;
+
     //Immediate control:
     case(opcode)
         7'd3: immediate_source = 2'b00;
@@ -25,42 +52,110 @@ always @ (posedge clock) begin
 
     //State machine
     case (state)
-    //instruction fetch 1: PC
-    4'b0000: begin
-    state = 4'b0001;
+    4'd0: begin // S0: Fetch
+        address_source = 1'b0;
+        ir_write = 1'b1;
+        ALU_source_A = 2'b00;
+        ALU_source_B = 2'b10;
+        ALU_operation = 2'b00;
+        result_source = 2'b10;
+        
+        state = 4'd1;
     end
 
-    //instruction fetch 2: fill Instruction Register
-    4'b0001: begin
-    ir_write = 1'b1;
-    state = 4'b0010; 
+    4'd1: begin // S1: Decode
+        ALU_source_A = 2'b01;
+        ALU_source_B = 2'b01;
+        ALU_operation = 2'b00;
+
+        case (opcode)
+            7'b0000011, 7'b0100011: state = 4'd2; // lw or sw 
+            7'b0110011: state = 4'd6; // R-type
+            7'b0010011: state = 4'd8; // I-type ALU
+            7'b1101011: state = 4'd9; // JAL
+            7'b1100011: state = 4'd10; // BEQ
+        endcase
     end
 
-    //sign extend and instruction decode state
-    4'b0010: begin
-    ir_write = 1'b0;
-    immediate_source = 1'b1; //VERIFICAR ESSA PORRA
-    if(opcode == LOADW_OP)begin
-        state = 4'b0011;
+    4'd2: begin // S2: MemAdr
+        ALU_source_A = 2'b00;
+        ALU_source_B = 2'b10;
+        ALU_operation = 2'b00;
+
+        case (opcode)
+            7'b0000011: state = 4'd3; // lw 
+            7'b0100011: state = 4'd5; // sw
+        endcase
+    end
+
+    4'd3: begin //S3: MemRead
+        result_source = 2'b00;
+        address_source = 1'b1;
+
+        state = 4'd4;
+    end
+
+    4'd4: begin //S4: MemWriteBack
+        result_source = 2'b01;
+        register_write = 1'b1;
+
+        state = 4'd0;
+    end
+
+    4'd5: begin //S5: MemWrite
+        result_source = 2'b00;
+        address_source = 1'b1;
+        memory_write = 1'b1;
+
+        state = 4'd0;
+        
+    end
+
+    4'd6: begin //S6: ExecuteR
+        ALU_source_A = 2'b10;
+        ALU_source_B = 2'b00;
+        ALU_operation = 2'b10;
+        
+        state = 4'd7; 
+    end
+
+    4'd7: begin //S7: ALU WriteBack
+        result_source = 2'b00;
+        register_write = 1'b1
+
+        state = 4'd0;
+    end
+
+    4'd8: begin //S8: ExecuteI
         ALU_source_A = 2'b10;
         ALU_source_B = 2'b01;
-
+        ALU_operation = 2'b10;
+        
+        state = 4'd7; 
     end
 
-    //LW step 1: 
-    4'b0001: begin
-    ir_write = 1'b1;
-    state = 4'b0010; 
+    4'd9: begin //S9: JAL
+        ALU_source_A = 2'b01;
+        ALU_source_B = 2'b10;
+        ALU_operation = 2'b00;
+        result_source = 2'b00;
+        pc_update = 1'b1;
+        
+        state = 4'd7; 
     end
 
+    4'd10: begin //s10: BEQ
+        ALU_source_A = 2'b10;
+        ALU_source_B = 2'b00;
+        ALU_operation = 2'b01;
+        result_source = 2'b00;
 
+        state = 4'd0; 
     end
 
-
-
+    default: state = 4'd0;
 
     endcase
-
 
 end
 
