3d8f21b7fc3419c365a8239552c0fcb930c3bebd
Gabriel Ayres <sowee00@gmail.com>
2025-03-27 01:54:35 -0300

    fixing syntax errors

--- immediate_extend.v
+++ immediate_extend.v
@@ -1,12 +1,17 @@
 module immediate_extend(
-    input [24:0] immediate,
-    input immediate_source,
+    input [31:0] full_instruction,
+    input [1:0] immediate_source,
     output reg [31:0] immediate_extended = 32'd0
 );
 
 always @ (immediate_source)
 begin
-    immediate_extended <= immediate;
+    case(immediate_source)
+        2'b00: immediate_extended = full_instruction[31:20];
+        2'b01: immediate_extended = {full_instruction[31:25],full_instruction[11:7]};
+        2'b10: immediate_extended = full_instruction[31:12];
+        2'b11: immediate_extended = 32'b1;
+    endcase
 end
 
 endmodule
