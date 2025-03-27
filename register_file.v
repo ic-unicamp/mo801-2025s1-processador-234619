03474e5418352a6e20d10dbdd227ab61b5aff9a1
Gabriel Ayres <sowee00@gmail.com>
2025-03-27 04:51:12 -0300

    fixing instantiation and assignments

--- register_file.v
+++ register_file.v
@@ -1,20 +1,26 @@
 module register_file(
-    input clock, write_enable,
-    input [4:0] rd, [4:0] rs1, [4:0] rs2,
+    input clock, 
+    input write_enable,
+
+    input [4:0] rd, 
+    input [4:0] rs1,
+    input [4:0] rs2,
+
     input [31:0] result,
-    output reg [31:0] rd1, [31:0] rd2
+
+    output reg [31:0] rd1, 
+    output reg [31:0] rd2
 );
 
-reg [4:0] registers [31:0]
+reg [4:0] registers [31:0];
 
 always @ (posedge clock) begin
 
     if (write_enable)
-        registers[rd] <= result;
+        registers[rd] = result;
     
-    rd1 <= registers[rs1]
-    rd2 <= registers[rs2]
-
+    rd1 = registers[rs1][31:0];
+    rd2 = registers[rs2][31:0];
 end
 
 endmodule
