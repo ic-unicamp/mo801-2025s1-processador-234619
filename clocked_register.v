b6963a09084978dde1b2feccee124e3751697990
Gabriel Ayres <sowee00@gmail.com>
2025-03-27 04:56:58 -0300

    fixing instantiation

--- clocked_register.v
+++ clocked_register.v
@@ -1,13 +1,16 @@
 module clocked_register(
-        input clock, enable,
-        input [31:0] inputA, [31:0] inputB,
-        output [31:0] outputA, [31:0] outputB
+        input clock, 
+        input enable,
+        input [31:0] inputA, 
+        input [31:0] inputB,
+        output reg [31:0] outputA, 
+        output reg [31:0] outputB
     );
     
     always @ (posedge clock) begin
         if (enable) begin
-            outputA <= inputA
-            outputB <= inputB
+            outputA = inputA;
+            outputB = inputB;
         end
     
     end
