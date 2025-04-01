5385e38c8b61084e5656d30cfcb8d10ead81d511
Gabriel Ayres <sowee00@gmail.com>
2025-04-01 02:57:32 -0300

    fixed all extensions

--- immediate_extend.v
+++ immediate_extend.v
@@ -11,9 +11,9 @@ end
 always @ (immediate_source)
 begin
     case(immediate_source)
-        2'b00: immediate_extended = full_instruction[31:20];
-        2'b01: immediate_extended = {full_instruction[31:25],full_instruction[11:7]};
-        2'b10: immediate_extended = full_instruction[31:12];
+        2'b00: immediate_extended = {{20{full_instruction[31]}}, full_instruction[31:20]};
+        2'b01: immediate_extended = {{20{full_instruction[31]}}, full_instruction[31:25], full_instruction[11:7]};
+        2'b10: immediate_extended = {{19{full_instruction[31]}}, full_instruction[31], full_instruction[7],full_instruction[30:25], full_instruction[11:8], 1'b0};
         2'b11: immediate_extended = 32'b1;
     endcase
 end
