163a05d6aca7da1e7600b44721818e373477656b
Rodolfo Azevedo <rodolfo.azevedo@unicamp.br>
2025-06-17 15:41:14 -0300

    arquivos de testes

--- run.sh
+++ run.sh
@@ -9,12 +9,14 @@ if ! iverilog -o tb *.v; then
 fi
 
 cp test/teste$1.mem memory.mem
-./tb | grep '===' > saida.out
+./tb > saida.out
+sed -i '' -n '/^M\[/p' saida.out
 cp saida.out test/saida$1.out
 cp saida.vcd test/saida$1.vcd
-#rm saida.out saida.vcd memory.mem
+rm saida.out saida.vcd memory.mem
 
-if diff --strip-trailing-cr test/saida$1.out test/saida$1.ok >/dev/null; then    echo "OK"
+if diff test/saida$1.out test/saida$1.ok >/dev/null; then
+    echo "OK"
     exit 0
 else
     echo "ERRO"
